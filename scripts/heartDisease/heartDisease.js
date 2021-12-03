966bef0b8a24af366c62ef72fb4b6441ca46e20a
Valentin Alecu <valentin.alecu@ibm.com>
2021-12-03 15:29:44 +0200

    animatieInima

--- scripts/heartDisease/heartDisease.js
+++ scripts/heartDisease/heartDisease.js
@@ -12,6 +12,24 @@ window.addEventListener('load', () => {
     canvas.height = 375;
     canvas.width = 630;
 
+    //INIMIOARA
+    const heart = document.getElementById('heart'); 
+    const para = document.getElementById('text');
+    const likeUnlikePost = function() {
+      if(heart.classList.contains('like')) {
+        heart.classList.remove('like');
+        heart.classList.add('unlike');
+        para.textContent = ':(';
+      } else {
+        heart.classList.remove('unlike');
+        heart.classList.add('like');
+        para.textContent = ':)';
+      }
+    }
+  
+    heart.addEventListener('click', likeUnlikePost); 
+    // INIMIOARA END
+
     initChart(context, yLabels);
 
     for (let i = 0; i < dataHeartDisease.length; i++) {
