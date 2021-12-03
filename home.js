07516772ce997be031d6dd0f6b8b2e6f2171ef4e
Anton <bogdan.anton@metroitpartner.com>
2021-12-04 01:42:23 +0200

    dbfd

--- home.js
+++ home.js
@@ -2,6 +2,16 @@ window.onload = init;
 
 function init(){
     mybutton = document.getElementById("myBtn");
+    let showGraphAudio = new Audio('./resources/soundEffects/navbar.mp3');
+    let navLinks = document.querySelectorAll('.nav-link');
+    let brand = document.querySelector('.navbar-brand');
+    console.log(brand);
+    brand.addEventListener('click', () => showGraphAudio.play());
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', () => {
+            showGraphAudio.play();
+        });
+    }
     
     window.onscroll = function() {scrollFunction()};
 
